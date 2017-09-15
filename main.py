c512e709b7ecd28bfca43a15f53aa19e3af30a88
Alexander Turivnoi <a.turivnoi@yandex.ru>
2017-09-15 13:09:47 +0300

    1st working version

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
+import config
+import grequests
+from flask.json import jsonify
 
 app = Flask(__name__)
 CORS(app)
@@ -7,8 +10,15 @@ app.config['SECRET_KEY'] = 'secret!'
 
 
 @app.route('/cluster_info')
-def info():
-    pass
+def cluster_info():
+    urls = config.SERVERS_LIST
+    shells = (grequests.get(u) for u in urls)
+    responses = grequests.map(shells)
+    result = {}
+    for r in responses:
+        result[r.url] = {'status': r.status_code, 'text': r.text}
+    return jsonify({'result': result})
+
 
 if __name__ == '__main__':
     app.run(debug=False, port=5500, host='0.0.0.0')
