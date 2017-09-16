94d427bf17e9db20d956c66781c1c29034a8a564
Alexander Turivnoi <a.turivnoi@yandex.ru>
2017-09-16 18:20:01 +0300

    error handling + refactoring

---
+++ data_fetcher.py
@@ -0,0 +1,27 @@
+import grequests
+
+
+class DataFetcher:
+
+    def __init__(self, servers_list):
+        """ Constructor
+         :param servers_list: List of cluster's server urls to make request for sysinfo
+        """
+        self.servers_list = servers_list
+        self.failed_servers = {}
+
+    def req_except_handler(self, request, exception):
+        self.failed_servers[request.url] = str(exception)
+
+    def get_info(self):
+            shells = (grequests.get(server) for server in self.servers_list)
+            responses = grequests.map(shells, exception_handler=self.req_except_handler)
+            result = {}
+            for r in responses:
+                try:
+                    result[r.url] = {'status': r.status_code, 'text': r.text}
+                except:
+                    pass
+            for failed in self.failed_servers:
+                result[failed] = {'status': self.failed_servers[failed]}
+            return result
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 import config
-import grequests
 from flask.json import jsonify
+from data_fetcher import DataFetcher
 
 app = Flask(__name__)
 CORS(app)
@@ -14,11 +14,8 @@ def cluster_info():
     clusters = config.CLUSTERS_LIST
     result = {}
     for k in clusters:
-        result[k] = {}
-        shells = (grequests.get(server) for server in clusters[k])
-        responses = grequests.map(shells)
-        for r in responses:
-            result[k][r.url] = {'status': r.status_code, 'text': r.text}
+        data_fetcher = DataFetcher(clusters[k])
+        result[k] = data_fetcher.get_info()
     return jsonify(result)
 
 if __name__ == '__main__':
